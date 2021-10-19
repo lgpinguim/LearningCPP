58a25eaa50dd46881c41a7b1f43ee5e21259f532
Luis Ferreira <37229605+lgpinguim@users.noreply.github.com>
2021-10-19 10:08:32 -0300

    First Project

---
+++ InputTest/InputTest.cpp
@@ -0,0 +1,15 @@
+// InputTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int number;
+    std::cout << "Enter a number:\n";
+    std::cin >> number;
+    std::cout << "Number received.\n";
+    
+
+    return 0;
+}
